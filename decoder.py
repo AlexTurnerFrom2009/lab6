693eead28a24bfa4ce170e76865e59615d198f8c
Brenden Reed <bs.reed@ufl.edu>
2024-10-23 19:18:02 -0400

    decoder changes

---
+++ decoder.py
@@ -0,0 +1,5 @@
+def decode(password2):
+    decoded = ''
+    for digit in password2:
+        decoded += str(int(digit)-3)[-1]
+    return decoded
--- encoder.py
+++ encoder.py
@@ -1,8 +1,8 @@
 # Wade Rogers
 
-def encode(password):
+def encode(password1):
   encoded = ""
-  for digit in password:
+  for digit in password1:
     encoded += str(int(digit)+3)[-1]
   return encoded
 
