9aaf9a911051747fb7d1658d4656af46cc9ec1fe
AlexTurnerFrom2009 <lwrmail2@gmail.com>
2024-10-23 16:51:25 +0000

    Added menu to main

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from encoder import encode
 from decoder import decode
 
-if __name__ == "__main__":
-  expected_encode = "45678888"
-  expected_decode = "12345555"
+if __name__ == '__main__':
+    password = ''
+    while True:
+        print('\nMenu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
 
-  assert encode("12345555") == expected_encode, "Encoder error"
-  assert decode("45678888") == expected_decode, "Decoder error"
-  
+        menu_option = input('Please enter an option: ')
+
+        if menu_option == '1':
+            password = input('Please enter your password to encode: ')
+            print('Your password has been encoded and stored!')
+        elif menu_option == '2':
+            print(f'The encoded password is {encode(password)}, and the original password is {decode(password)}.')
+        elif menu_option == '3':
+            break
